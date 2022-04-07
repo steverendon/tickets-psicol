9172c6f62b061f9854b4c214b4ce171610dffa60
Steven Rendon <steven.rendon@hotmail.com>
2022-04-07 07:14:06 -0500

    refactoring

---
+++ config.example.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'database' => [
+        'host'      => 'localhost',
+        'database'  => 'psicol',
+        'password'  => '123456',
+        'username'  => 'admin'
+    ]
+    
+];
+
